b53e5147eff917f79e3c20055819096e835cdbce
cbarbello <caseyjbarbello@gmail.com>
2014-08-28 16:46:34 -0700

    Added professor names to an array

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-var cells = getElementsByClassName('clcellsecondary');
-for (var i=0; i<cells.length; i++) {
-	cells[i].style.color = 'red';
+var cells = document.getElementsByClassName('clcellprimary');
+var professors = [];
+//increment list to retrieve professor name only
+for (var i=3; i<cells.length; i+=6) {
+	cells[i].style.color = 'red'; //test
+	professors.push(cells[i].innerText);
+	cells[i].innerText = professors[0]; //test
 }
+
