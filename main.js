78cf55e0eac54f1fbde559573c54fe8167d511a1
cbarbello <caseyjbarbello@gmail.com>
2014-09-01 23:35:57 -0700

    Added buttons and links

--- main.js
+++ main.js
@@ -1,23 +1,27 @@
 var cells = document.getElementsByClassName('clcellprimary');
 var professors = [];
+var profCount = 0;
+
 
 //increment list to retrieve professor name only
 for (var i=3; i<cells.length; i+=18) {
-	//if (cells[i].innerText != 'T.B.A. &nbsp'){
-		cells[i].style.color = 'red'; //test
-		professors.push(cells[i].innerText);
-		cells[i].innerText = professors[0]; //test
-		cells[i+9].innerText = 'Rating:';
-	//}
+	//slice off &nbsp; character
+	var profName = cells[i].innerText.slice(0,-1);	
+	if (profName != 'T.B.A.' && profName != 'Cancel'){
+
+		//slice off remaining space at end and push to professor array
+		professors.push(profName.slice(0,-1));
+		
+		//create link to professor rating
+		link = document.createElement('a');
+		//link.className = 'ratingLink';
+		var searchName = professors[profCount].split(' ')[0];
+		link.href = 'http://www.ratemyprofessors.com/SelectTeacher.jsp?searchName=' + searchName + '&search_submit1=Search&sid=1077';
+		link.innerHTML = '<input class="rating" type="button" value="Rating" />';
+
+		cells[i+9].appendChild(link);
+		
+		profCount++;
+	}
 
 }
-
-
-
-
-
-/*
-var xhr = new XMLHttpRequest();
-xhr.open('GET', 'http://www.ratemyprofessors.com/SelectTeacher.jsp?sid=1077', false);
-xhr.getElementsByClassName('searchBox').value = professors[0];
-*/
