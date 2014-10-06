69ad0a74bf1f07e31dbbae1f007490997afcbd81
cbarbello <caseyjbarbello@gmail.com>
2014-10-06 15:59:42 -0700

    styled ratings button

--- main.js
+++ main.js
@@ -16,13 +16,13 @@ function main() {
 			var div = cells[i+9];
 			var searchName = professors[profCount].split(' ')[0];
 			div.url = 'http://www.ratemyprofessors.com/SelectTeacher.jsp?searchName=' + searchName + '&search_submit1=Search&sid=1077';
-			div.innerHTML = '<input class="rating" type="button" value="Show Rating" />';
+			div.innerHTML = '<input class="ratingButton" type="button" value="Show Rating" />';
 			div.cell = cells[i+10];
 			div.clicked = false;
 			div.addEventListener('click', openPopup);
 
 			profCount++;
-		}
+		}//end if
 	}
 
 	//action on button click 
@@ -30,13 +30,13 @@ function main() {
 		if (this.clicked == true) {
 			//remove popup
 			this.cell.innerHTML = '';
-			this.innerHTML = '<input class="rating" type="button" value="Show Rating" />';
+			this.innerHTML = '<input class="ratingButton" type="button" value="Show Rating" />';
 			this.clicked = false;
 		}
 		else{
 			//change the button to say "Hide Rating"
 			this.clicked = true;
-			this.innerHTML = '<input class="rating" type="button" value="Hide Rating" />';
+			this.innerHTML = '<input class="ratingButton" type="button" value="Hide Rating" />';
 
 			//create and show popup
 			var popup = document.createElement('div');
@@ -55,32 +55,17 @@ function main() {
   				if (xhr.readyState==4 && xhr.status==200) {
    				var data = xhr.response;
    				this.popup.innerHTML = data;
-   				var test = document.getElementsByClassName('profName');
-   				//test[2] = test[2].getElementsByTagName('a');
+   				var test = document.getElementsByClassName('profName');//causing error when prof search turns up null
+
    				this.popup.innerHTML = test[2].innerHTML;
+
+
     			}
   			}
-  			xhr.send();
-		}
-	}
-
-/*
-	var xhr = new XMLHttpRequest();
-	var url = 'http://www.ratemyprofessors.com/SelectTeacher.jsp?sid=1077';
-	xhr.open('GET', url, true);
-
-	xhr.onreadystatechange=function() {
-  		if (xhr.readyState==4 && xhr.status==200)
-   		{
-   			var data = xhr.response;
-   			cells[3].innerHTML = data;
-    	}
-    	else{
-   		}
-  	}
-  	xhr.send();
-*/
-};
+  		xhr.send();
+	}//end else
+	}//end openPopup()
+};//end main
 
 
 main();
