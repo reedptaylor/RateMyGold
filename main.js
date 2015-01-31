d256e5b9a7b0b48b42cb0f04a2fbb51c76b762e6
Casey Barbello <caseyjbarbello@gmail.com>
2015-01-31 07:12:09 -0800

    BEASTMODE: sorted data, made divs, and added comments

--- main.js
+++ main.js
@@ -4,16 +4,16 @@ function main() {
 	var professors = [];
 	var profCount  = 0;
 
-	for (var i = 3; i < length; i += 18) {
+	for (var i = 3; i < length; i += 18) { //iterate through the cells, only those which contain a professor name
 		var profName = cells[i].innerText.slice(0,-1); //slice off &nbsp; character	
 		if (profName != 'T.B.A.' && profName != 'Cancel'){
 			professors.push(profName.slice(0,-1)); //slice off remaining space at end and push to professor array
-			var div        = cells[i+9];
-			var searchName = professors[profCount].split(' ')[0];
+			var div        = cells[i+9]; //cell where the button will go
+			var searchName = professors[profCount].split(' ')[0]; //slice off all characters except last name
 			div.searchURL  = 'http://www.ratemyprofessors.com/search.jsp?queryBy=teacherName&schoolName=university+of+california+santa+barbara&queryoption=HEADER&query='+ searchName +'&facetSearch=true';
 			div.profURL    = '';
 			div.innerHTML  = '<input class="ratingButton" type="button" value="SHOW RATING" />';
-			div.cell       = cells[i+10];
+			div.cell       = cells[i+10]; //cell where the popup's html will be placed
 			div.clicked    = false;
 			div.addEventListener('click', openPopup);
 			profCount++;
@@ -22,12 +22,12 @@ function main() {
 }
 
 function openPopup() {
-	if (this.clicked == true) {
+	if (this.clicked == true) { //happens when button was clicked while active
 		this.cell.innerHTML = '';
 		this.innerHTML      = '<input class="ratingButton" type="button" value="SHOW RATING" />';
 		this.clicked        = false;
 	}
-	else {
+	else { //happens when button was clicked while inactive
 		this.clicked    = true;
 		this.innerHTML  = '<input class="ratingButton" type="button" value="HIDE RATING" />';	
 		var popup       = document.createElement('div');
@@ -36,26 +36,69 @@ function openPopup() {
 		this.cell.style.position = 'relative';
 		this.cell.appendChild(popup);
 
-		chrome.runtime.sendMessage({
+		chrome.runtime.sendMessage({ //need a separate event page to do the xmlhttprequest because of http to https issue
     		url: this.searchURL,
 		}, function(responseText) {
 			var tmp        = document.createElement('div');//make a temp element so that we can search through its html
    			tmp.innerHTML  = responseText;
-   			var foundProfs = tmp.getElementsByClassName('listing PROFESSOR'); //throw exception here if null
-   			tmp.innerHTML  = foundProfs[0].innerHTML;
-   			var link       = tmp.getElementsByTagName('a');
-   			this.profURL   = 'http://www.ratemyprofessors.com/' + link[0].toString().slice(23); //this is the URL
+   			var foundProfs = tmp.getElementsByClassName('listing PROFESSOR'); 
    			
+   			if (foundProfs.length == 0){ //if no results were returned, print this message
+   				popup.innerText = "Professor has no ratings.";
+   			}
+   			else{
+   				tmp.innerHTML  = foundProfs[0].innerHTML;
+   				var link       = tmp.getElementsByTagName('a');
+   				this.profURL   = 'http://www.ratemyprofessors.com/' + link[0].toString().slice(23); //this is the URL
 
    				chrome.runtime.sendMessage({
     				url: this.profURL,
 				}, function(responseText) {
-					var tmp         = document.createElement('div');//make a temp element so that we can search through its html
+					tmp         = document.createElement('div');
    					tmp.innerHTML   = responseText;
-   					var foundProfs  = tmp.getElementsByClassName('rating-breakdown');
-   					popup.innerHTML = foundProfs[0].innerHTML;
-				});
+   					var ratingInfo  = tmp.getElementsByClassName('left-breakdown')[0];
+   					tmp.innerHTML   = ratingInfo.innerHTML;
 
+   					//get the raw rating data
+   					var overallAndAvg = tmp.getElementsByClassName('grade');
+   					var otherRatings  = tmp.getElementsByClassName('rating');
+   					var overall       = overallAndAvg[0];
+   					var avgGrade      = overallAndAvg[1];
+   					var helpfulness   = otherRatings[0];
+   					var clarity       = otherRatings[1];
+   					var easiness      = otherRatings[2];
+   					tmp.remove();
+ 
+   					//create the ratings divs
+   					var overallDiv     = document.createElement('div');
+					var avgGradeDiv    = document.createElement('div');
+					var helpfulnessDiv = document.createElement('div');
+					var clarityDiv     = document.createElement('div');
+					var easinessDiv    = document.createElement('div');
+
+					//assign class names for styling
+					overallDiv.className     = 'rating';
+					avgGradeDiv.className    = 'rating';
+					helpfulnessDiv.className = 'rating';
+					clarityDiv.className     = 'rating';
+					easinessDiv.className    = 'rating';
+
+					//put rating data in divs
+					overallDiv.innerText     = 'Overall Quality: ' + overall.innerHTML;
+					avgGradeDiv.innerText    = 'Average Grade: '   + avgGrade.innerHTML;
+					helpfulnessDiv.innerText = 'Helpfulness: '     + helpfulness.innerHTML;
+					clarityDiv.innerText     = 'Clarity: '         + clarity.innerHTML;
+					easinessDiv.innerText    = 'Easiness: '        + easiness.innerHTML;
+
+					//add divs to popup
+   					popup.appendChild(overallDiv);
+   					popup.appendChild(avgGradeDiv);
+   					popup.appendChild(helpfulnessDiv);
+   					popup.appendChild(clarityDiv);
+   					popup.appendChild(easinessDiv);
+
+				});
+			}
 
 		});
 	}
